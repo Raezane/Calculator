35c393da110d0e592992697202d17c6d53b0a71e
Antti Räsänen <143749752+Raezane@users.noreply.github.com>
2024-01-09 20:24:44 +0200

    write html structure

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let firstNum;
+let operator;
+let nextNum;
+
+function add (a, b) {
+
+}
+
+function subtract (a, b) {
+    
+}
+
+function multiply (a, b) {
+    
+}
+
+function divide (a, b) {
+    
+}
+
+function operate(firstNum, operator, nextNum) {
+    
+}
